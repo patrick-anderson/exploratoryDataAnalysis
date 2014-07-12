29b835e3c83a01bb165b8a40edb20eaeed73f1ee
patrick-anderson <patrick.anderson@cdph.ca.gov>
2014-07-12 09:42:52 -0700

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,15 +1,26 @@
-data=data.frame(matrix(unlist(sapply(d,strsplit,";")),ncol=9,byrow=T),stringsAsFactors =F)
+## Getting full dataset
+data_full <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
 
+## Subsetting the data
+data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data_full)
 
-colnames(data)=c("Date","Time","Global_active_power","Global_reactive_power",
-                 "Voltage","Global_intensity","Sub_metering_1","Sub_metering_2",
-                 "Sub_metering_3")
-p=strptime(paste(data[,1],data[,2]),format= "%d/%m/%Y %H:%M:%S")
-data$DT=p
-head(data)
-png(file="plot3.png",width=480,height=480)
-plot(data$DT,as.numeric(data$Sub_metering_1),type="l",ylab="Energy sub metering",xlab="")
-lines(data$DT,as.numeric(data$Sub_metering_2),col=2)
-lines(data$DT,as.numeric(data$Sub_metering_3),col=4)
-legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c(1,2,4),lty=1)
+## Converting dates
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+## Plot 3
+with(data, {
+    plot(Sub_metering_1~Datetime, type="l",
+         ylab="Global Active Power (kilowatts)", xlab="")
+    lines(Sub_metering_2~Datetime,col='Red')
+    lines(Sub_metering_3~Datetime,col='Blue')
+})
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+## Saving to file
+dev.copy(png, file="plot3.png", height=480, width=480)
 dev.off()
