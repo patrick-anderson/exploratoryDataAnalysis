406670df754c8f902bcfc8638633aa90e9ff1a80
patrick-anderson <patrick.anderson@cdph.ca.gov>
2014-07-12 09:43:59 -0700

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,31 +1,33 @@
-d=scan("household_power_consumption.txt", what=character(),na.strings="?", skip=66637, nlines=2880)
-data=data.frame(matrix(unlist(sapply(d,strsplit,";")),ncol=9,byrow=T),stringsAsFactors =F)
+## Getting full dataset
+data_full <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
 
-d=scan("household_power_consumption.txt", what=character(),na.strings="?", skip=66637, nlines=2880)
+## Subsetting the data
+data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data_full)
 
+## Converting dates
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
 
-colnames(data)=c("Date","Time","Global_active_power","Global_reactive_power",
-                 "Voltage","Global_intensity","Sub_metering_1","Sub_metering_2",
-                 "Sub_metering_3")
-p=strptime(paste(data[,1],data[,2]),format= "%d/%m/%Y %H:%M:%S")
-data$DT=p
+## Plot 4
+par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+with(data, {
+    plot(Global_active_power~Datetime, type="l", 
+         ylab="Global Active Power (kilowatts)", xlab="")
+    plot(Voltage~Datetime, type="l", 
+         ylab="Voltage (volt)", xlab="")
+    plot(Sub_metering_1~Datetime, type="l", 
+         ylab="Global Active Power (kilowatts)", xlab="")
+    lines(Sub_metering_2~Datetime,col='Red')
+    lines(Sub_metering_3~Datetime,col='Blue')
+    legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+           legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+    plot(Global_reactive_power~Datetime, type="l", 
+         ylab="Global Rective Power (kilowatts)",xlab="")
+})
 
-
-png(file="plot4.png",width=480,height=480)
-par(mfcol=c(2,2))
-plot(data$DT,as.numeric(data$Global_active_power),type="l",ylab="Global active power",xlab="")
-
-
-plot(data$DT,as.numeric(data$Sub_metering_1),type="l",ylab="Energy sub metering",xlab="")
-lines(data$DT,as.numeric(data$Sub_metering_2),col=2)
-lines(data$DT,as.numeric(data$Sub_metering_3),col=4)
-legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c(1,2,4),lty=1,bty = "n")
-
-
-plot(data$DT,as.numeric(data$Voltage),type="l",ylab="Voltage",xlab="datetime")
-
-
-plot(data$DT,as.numeric(data$Global_reactive_power),type="l",ylab="Global_reactive_power",xlab="datetime")
-
-
+## Saving to file
+dev.copy(png, file="plot4.png", height=480, width=480)
 dev.off()
