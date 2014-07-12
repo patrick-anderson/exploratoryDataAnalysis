91b9a78bb8abbeff211bb6bf53e1efd873c998ef
patrick-anderson <patrick.anderson@cdph.ca.gov>
2014-07-12 09:40:37 -0700

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,13 +1,20 @@
-d=scan("household_power_consumption.txt", what=character(),na.strings="?", skip=66637, nlines=2880)
-data=data.frame(matrix(unlist(sapply(d,strsplit,";")),ncol=9,byrow=T),stringsAsFactors =F)
+## Getting full dataset
+data_full <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
 
+## Subsetting the data
+data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data_full)
 
-colnames(data)=c("Date","Time","Global_active_power","Global_reactive_power",
-                 "Voltage","Global_intensity","Sub_metering_1","Sub_metering_2",
-                 "Sub_metering_3")
-p=strptime(paste(data[,1],data[,2]),format= "%d/%m/%Y %H:%M:%S")
-data$DT=p
-head(data)
-png(file="plot1.png",width=480,height=480)
-hist(as.numeric(data$Global_active_power),main="Global active power",xlab="Global active power (kilowatts)",col=2)
+## Converting dates
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+## Plot 1
+hist(data$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+## Saving to file
+dev.copy(png, file="plot1.png", height=480, width=480)
 dev.off()
